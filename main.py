a1fb1106dc2a22db2c4b20afddb9031c46dde395
MalerBrash <malerbrash@rambler.ru>
2021-07-13 00:48:24 +0400

    bugs fixed

--- main.py
+++ main.py
@@ -4,43 +4,44 @@ import hashlib
 import os
 
 
-class IterCounter:          
-    def __init__(self, file):        
+class IterCounter:
+    def __init__(self, file):
         self.file = open(file, 'r', encoding='utf-8')
 
-    def __iter__(self):      
-        return self
+    def __iter__(self):
+        link_list = []
+        for i in iter(json.load(self.file)):
+            for k, v in i.items():
+                if k == 'name':
+                    for k, v in v.items():
+                        if k == 'common':
+                            link = 'https://en.wikipedia.org/wiki/' + v.replace(' ', '_') + '\n'
+                            link_list.append(link)
+                            self.counter_writer(v + ', ' + link)  # вызывает запись страны и ссылки в файл
+
+        return iter(link_list)
 
     def __next__(self):
-      d = next(self.g)
-      return d
+        link = next(self)
+        return link
 
-    def counter_writer(self, country):
+    def counter_writer(self, link):
         with open('json_file.txt', 'a', encoding='utf-8') as f:
-            f.write(country + ', https://en.wikipedia.org/wiki/' + country.replace(' ', '_') + '\n')
-          
-    def start_iter(self):
-        for i in iter(json.load(self.file)): 
-            for k,v in i.items():
-                if k == 'name':
-                    for k,v in v.items():
-                        if k == 'common':
-                            self.counter_writer(v)
+            f.write(link)
 
 
-def MyHashGen(file_path):     # хэш генератор                   
+def MyHashGen(file_path):  # хэш генератор
     with open(file_path, 'r', encoding='utf-8') as f:
         for i, v in enumerate(f):
             yield hashlib.md5(v.encode()).hexdigest()
-                     
-       
+
+
 file = 'json_file.txt'
 file_path = os.path.join(os.getcwd(), 'ad_files', file)
 
- 
 if __name__ == '__main__':
-    f = IterCounter('countries.json')
-    f.start_iter()                       ####### DZ 1
+    for item in IterCounter('countries.json'):
+        print(item)  ####### DZ 1
 
     for hash in MyHashGen(file):
-        print(hash)                      ####### DZ 2
+        print(hash)  ####### DZ 2
